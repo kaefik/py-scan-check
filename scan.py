0bcce2837bbfe36abef586c88993b21e36886ea9
oilnur <oilnur@oilnur-nout.lan>
2018-03-27 21:23:18 +0300

    сделана возможность скрипту scan.py передавать аргументы фискального накопителя, фисальные данные, фискальный признак документа

--- scan.py
+++ scan.py
@@ -16,30 +16,39 @@ ZZZZZZZZZZ - фискальный признак документа(ФПД)
 import requests
 import base64
 
+import click
 from  config import Config
 
-cfg = Config()
-strs = "{0}:{1}".format(cfg.username, cfg.code)
-# print(strs)
-encoded_authorization = base64.b64encode(strs.encode())
-str_encoded_authorization = "Basic {0}".format(encoded_authorization.decode("utf-8"))
+# python3 scan.py 8710000100730663 2860 4249261172
 
-"""
-fn = '8710000100730663' # номер фискального накопителя (ФН)
-fd = '2860'             # фискальные данные(ФД)
-fpd = '4249261172'      # фискальный признак документа(ФПД)
-"""
+@click.command()
+@click.argument('fn')
+@click.argument('fd')
+@click.argument('fpd')
+def main(fn, fd, fpd):
+    cfg = Config()
+    strs = "{0}:{1}".format(cfg.username, cfg.code)
+    # print(strs)
+    encoded_authorization = base64.b64encode(strs.encode())
+    str_encoded_authorization = "Basic {0}".format(encoded_authorization.decode("utf-8"))
 
-fn = '8710000100677412' # номер фискального накопителя (ФН)
-fd = '93529'             # фискальные данные(ФД)
-fpd = '2689912220'      # фискальный признак документа(ФПД)
+    """
+    fn = '8710000100730663' # номер фискального накопителя (ФН)
+    fd = '2860'             # фискальные данные(ФД)
+    fpd = '4249261172'      # фискальный признак документа(ФПД)
 
+    fn = '8710000100677412' # номер фискального накопителя (ФН)
+    fd = '93529'             # фискальные данные(ФД)
+    fpd = '2689912220'      # фискальный признак документа(ФПД)
+    """
 
-url = "http://proverkacheka.nalog.ru:8888/v1/inns/*/kkts/*/fss/{0}/tickets/{1}?fiscalSign={2}&sendToEmail=no".format(fn, fd, fpd)
+    url = "http://proverkacheka.nalog.ru:8888/v1/inns/*/kkts/*/fss/{0}/tickets/{1}?fiscalSign={2}&sendToEmail=no".format(fn, fd, fpd)
 
-header_session = {'Authorization': str_encoded_authorization, 'User-Agent': 'okhttp/3.0.1', 'Device-Id': 'android_id', 'Device-OS': 'Adnroid 6.0.1', 'Version': '2', 'ClientVersion': '1.4.2', 'Host': 'proverkacheka.nalog.ru:8888' }
+    header_session = {'Authorization': str_encoded_authorization, 'User-Agent': 'okhttp/3.0.1', 'Device-Id': 'android_id', 'Device-OS': 'Adnroid 6.0.1', 'Version': '2', 'ClientVersion': '1.4.2', 'Host': 'proverkacheka.nalog.ru:8888' }
 
-r = requests.get(url, headers=header_session) 
+    r = requests.get(url, headers=header_session) 
 
-print(r)
-print(r.json())
+    print(r)
+    print(r.json())
+if __name__=="__main__":
+    main()
